11c80ee0fc83c8e8193f476278590b1857818a12
PythonUser112 <buddha@sbs-braungardt.de>
2026-09-05 17:00:36 +0200

    Player health duplicates removed

--- game/Player.gd
+++ game/Player.gd
@@ -1,7 +1,5 @@
 extends "res://game/Character.gd"
 
-export (int) var lives
-
 func _process(_delta):
 	for dir in directions:
 		if Input.is_action_pressed(dir):
--- tiles/AppleTree/Apple.gd
+++ tiles/AppleTree/Apple.gd
@@ -2,4 +2,4 @@ extends Area2D
 
 func _on_Apple_area_entered(area: Area2D):
 	if area.is_in_group("Player"):
-		area.lives += 1
+		area.hp += 1

6b87b15f90e10bff2f345a404b2630ded29f3448
PythonUser112 <buddha@sbs-braungardt.de>
2026-09-05 15:24:41 +0200

    Character lives added

--- game/Player.gd
+++ game/Player.gd
@@ -1,5 +1,7 @@
 extends "res://game/Character.gd"
 
+export (int) var lives
+
 func _process(_delta):
 	for dir in directions:
 		if Input.is_action_pressed(dir):
--- tiles/AppleTree/Apple.gd
+++ tiles/AppleTree/Apple.gd
@@ -1,2 +1,5 @@
 extends Area2D
 
+func _on_Apple_area_entered(area: Area2D):
+	if area.is_in_group("Player"):
+		area.lives += 1

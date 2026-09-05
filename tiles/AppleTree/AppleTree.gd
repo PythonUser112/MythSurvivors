4601128ec12ea4ae333e5582ada2ef0c518a6481
PythonUser112 <buddha@sbs-braungardt.de>
2026-09-05 15:15:45 +0200

    sprites added

--- tiles/AppleTree/AppleTree.gd
+++ tiles/AppleTree/AppleTree.gd
@@ -9,4 +9,9 @@ func _ready():
 func _on_AppleTree_area_entered(area: Area2D):
 	if area.is_in_group("PlayerWeapons") and loaded:
 		$Sprite.texture = preload("res://tiles/AppleTree/Tree.png")
-		push_error("Npo apple-dropping method yet!")
+		var apple1 = preload("res://tiles/AppleTree/Apple.tscn").instance()
+		get_parent().add_child(apple1)
+		apple1.position = position + Vector2(16, 20)
+		var apple2 = preload("res://tiles/AppleTree/Apple.tscn").instance()
+		get_parent().add_child(apple2)
+		apple2.position = position + Vector2(16, 20)
